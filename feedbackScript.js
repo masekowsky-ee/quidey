24adcd919683fadea08a20cc674593d64a7bcfb7
Caligula09 <eesgema@gmail.com>
2026-04-20 19:29:04 -0400

    feedback and feedback menu

---
+++ feedbackScript.js
@@ -0,0 +1,39 @@
+const openMenu = () => {
+    document.getElementById('menu').classList.add('open');
+}
+
+const closeMenu = () => {
+    document.getElementById('menu').classList.remove('open');
+}
+
+//open menu
+document.getElementById("menuIconImg").addEventListener("click", openMenu);
+//close menu
+['menuHeadH1', 'menuHeadImg'].forEach(id => {
+    document.getElementById(id).addEventListener('click', closeMenu);
+})
+
+//send feedback
+document.getElementById('sendFeedback').addEventListener('click', async () => {
+    const feedback = document.getElementById('feedback').value;
+
+    if (!feedback.trim()){
+        alert('Bitte etwas eingeben');
+        return;
+    }
+
+    await fetch('https://formspree.io/f/xpqkjdqn', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            message: feedback
+        })
+    });
+
+    alert('Danke für dein Feedback');
+    document.getElementById('feedback').value = "";
+});
+
+console.log(document.getElementById("menuIconImg"));
